e94cd2b146ac71a72a1a5dda1c2c27f87a113840
Santiago Aguirre Giraldo <santiago@Santiagos-MacBook-Pro.local>
2023-05-16 10:28:16 -0500

    Add React hook named useContext

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { TodoCounter } from '../TodoCounter';
 import { TodoSearch } from '../TodoSearch';
 import { TodoList } from '../TodoList';
@@ -6,28 +7,21 @@ import { TodosLoading } from '../TodosLoading';
 import { TodosError } from '../TodosError';
 import { EmptyTodos } from '../EmptyTodos';
 import { CreateTodoButton } from '../CreateTodoButton';
+import { TodoContext } from '../TodoContext';
 
-function AppUI({
-  loading,
-  error,
-  completedTodos,
-  totalTodos,
-  searchValue,
-  setSearchValue,
-  searchedTodos,
-  completeTodo,
-  deleteTodo,
-}) {
+function AppUI() {
+  const {
+      loading,
+      error,
+      searchedTodos,
+      completeTodo,
+      deleteTodo,
+  } = React.useContext(TodoContext);
+
   return (
     <>
-      <TodoCounter
-        completed={completedTodos}
-        total={totalTodos} 
-      />
-      <TodoSearch
-        searchValue={searchValue}
-        setSearchValue={setSearchValue}
-      />
+      <TodoCounter />
+      <TodoSearch />
 
       <TodoList>
         {loading && (
@@ -50,7 +44,7 @@ function AppUI({
           />
         ))}
       </TodoList>
-      
+
       <CreateTodoButton />
     </>
   );
