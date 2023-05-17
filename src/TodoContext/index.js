bc5c6ea8f54ab17cf8cd8535776dfe324965ee27
Santiago Aguirre Giraldo <santiago@Santiagos-MacBook-Pro.local>
2023-05-17 13:03:22 -0500

    Finished

--- src/TodoContext/index.js
+++ src/TodoContext/index.js
@@ -12,7 +12,7 @@ function TodoProvider({ children }) {
     error,
   } = useLocalStorage('TODOS_V1', []);
   const [searchValue, setSearchValue] = React.useState('');
-  const [openModal, setOpenModal] = React.useState(true);
+  const [openModal, setOpenModal] = React.useState(false);
 
   const completedTodos = todos.filter(
     todo => !!todo.completed
@@ -26,6 +26,15 @@ function TodoProvider({ children }) {
       return todoText.includes(searchText);
     }
   );
+
+  const addTodo = (text) => {
+    const newTodos = [...todos];
+    newTodos.push({
+      text,
+      completed: false,
+    });
+    saveTodos(newTodos);
+  };
 
   const completeTodo = (text) => {
     const newTodos = [...todos];
@@ -54,6 +63,7 @@ function TodoProvider({ children }) {
       searchValue,
       setSearchValue,
       searchedTodos,
+      addTodo,
       completeTodo,
       deleteTodo,
       openModal,
